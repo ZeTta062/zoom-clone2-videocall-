c7297f495419f08ab008f6d93d4f2b47df0a90cf
ZeTta09 <samhak17@naver.com>
2024-12-29 21:22:20 +0900

    video active

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,43 @@
 const frontSocket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            {
+                audio:true,
+                video:true,
+            });
+            myFace.srcObject = myFace;
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+getMedia();
+
+muteBtn.addEventListener("click", () => {
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "mute";
+        muted = false;
+    }
+});
+cameraBtn.addEventListener("click", () => {
+    if(!cameraOff){
+        cameraBtn.innerText = "Camera On";
+        cameraOff = true;
+    } else {
+        cameraBtn.innerText = "Camera Off";
+        cameraOff = false;
+    }
+}); 
--- src/server.js
+++ src/server.js
@@ -15,6 +15,9 @@ app.get("/*", (req, res) => res.redirect("/")); //CATCHALL URL
 const httpServer = http.createServer(app);          //express
 const wsServer = new Server(httpServer);
 
+
+
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
 httpServer.listen(3000, handleListen);
 
