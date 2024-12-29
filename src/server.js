0efb46eb346e92e87c0b24af246af87cb90b9aa3
ZeTta09 <samhak17@naver.com>
2024-12-29 19:29:41 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); //CATCHALL URL
+
+
+const httpServer = http.createServer(app);          //express
+const wsServer = new Server(httpServer);
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+httpServer.listen(3000, handleListen);
+
+
