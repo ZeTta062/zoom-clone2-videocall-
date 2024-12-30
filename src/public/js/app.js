8ff7cd6fe45000af5cfe0fc8aed588e12adab0ed
ZeTta09 <samhak17@naver.com>
2024-12-30 18:23:36 +0900

    call controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,19 +3,38 @@ const frontSocket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras")
 
 let myStream;
 let muted = false;
-let cameraOff = false;
+let cameraOff = true;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        console.log(devices);
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        console.log(cameras);
+        cameras.forEach(camera => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 async function getMedia() {
     try {
         myStream = await navigator.mediaDevices.getUserMedia(
             {
                 audio:true,
-                video:true,
+                video:false,
             });
-            myFace.srcObject = myFace;
+            myFace.srcObject = myStream;
+            await getCameras();
     } catch(error) {
         console.log(error)
     }
@@ -24,6 +43,9 @@ async function getMedia() {
 getMedia();
 
 muteBtn.addEventListener("click", () => {
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled =! track.enabled));
     if(!muted){
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -33,11 +55,14 @@ muteBtn.addEventListener("click", () => {
     }
 });
 cameraBtn.addEventListener("click", () => {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled =! track.enabled));
     if(!cameraOff){
-        cameraBtn.innerText = "Camera On";
+        cameraBtn.innerText = "Camera Off";
         cameraOff = true;
     } else {
-        cameraBtn.innerText = "Camera Off";
+        cameraBtn.innerText = "Camera On";
         cameraOff = false;
     }
 }); 
